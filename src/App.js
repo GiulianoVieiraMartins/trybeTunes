deb38f5367d24a4d00dd69428423159b0d7a2e24
Giuliano <giulianovieiramartins@gmail.com>
2022-10-03 14:49:06 -0300

    adicionando as rotas, requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>Trybetunes</p>);
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/Favorites" component={ Favorites } />
+        <Route path="/Profile" component={ Profile } />
+        <Route path="/Profile/edit" component={ ProfileEdit } />
+        {/* falta a componente NotFound */}
+      </BrowserRouter>
+    );
   }
 }
 
