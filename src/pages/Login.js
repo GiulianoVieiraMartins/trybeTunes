0b6cfb040c67a023d3ccaea2206cdf8e9cef1e9d
Giuliano <giulianovieiramartins@gmail.com>
2022-10-03 15:30:47 -0300

    fazendo o login e adição do switch no app

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <input testid="login-name-input" />
+        <button type="button">Entrar</button>
+      </div>
     );
   }
 }
