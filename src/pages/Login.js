934b256b8449d18ee166c28ee72b554ceaba6bdf
Giuliano <giulianovieiramartins@gmail.com>
2022-10-03 23:26:25 -0300

    requisitos 2 e 4 passando

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { createUser } from '../services/userAPI';
+import Loading from '../Components/Loading';
 
 export default class Login extends Component {
   constructor() {
     super();
     this.state = {
       campoLogin: '',
+      loading: false,
     };
   }
-
-  // verifica = () => {
-  //   this.state.campoLogin.length > 3;
-  // };
 
   handleChange = ({ target }) => {
     const { name, value } = target;
@@ -20,29 +19,44 @@ export default class Login extends Component {
     });
   };
 
+  handleSubmit = async () => {
+    const { campoLogin } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name: campoLogin });
+    // this.setState({ loading: false });
+    history.push('search');
+  };
+
   render() {
     const tres = 3;
-    const { campoLogin } = this.state;
+    const { campoLogin, loading } = this.state;
+    // renderCondicional = () => { loading ? (<Loading />) : (form); };
     return (
-      <div data-testid="page-login">
-        <input
-          value={ campoLogin }
-          name="campoLogin"
-          data-testid="login-name-input"
-          onChange={ this.handleChange }
-        />
-        <button
-          data-testid="login-submit-button"
-          name="loginButton"
-          type="button"
-          disabled={ campoLogin.length < tres }
-          onClick={ createUser({ name: campoLogin }) }
-          // pq o teste não passa a função sendo chamda e onde que essa variavel esta sendo guardada?
-        >
-          Entrar
-
-        </button>
-      </div>
+      loading ? (<Loading />) : (
+        <div data-testid="page-login">
+          <input
+            value={ campoLogin }
+            name="campoLogin"
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="login-submit-button"
+            name="loginButton"
+            type="button"
+            disabled={ campoLogin.length < tres }
+            onClick={ this.handleSubmit }
+          >
+            Entrar
+          </button>
+        </div>)
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
