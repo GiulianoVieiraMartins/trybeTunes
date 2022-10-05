dea806c7d0d681fde3e501fab9bc45449bcf6e3f
Giuliano <giulianovieiramartins@gmail.com>
2022-10-05 16:59:05 -0300

    requisito 7 dos infernos

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      retornoAPI: [],
+    };
+  }
+
   async componentDidMount() {
-    const { id } = this.props.match.params;
+    const { match } = this.props;
+    const { id } = match.params;
     const data = await getMusics(id);
-    console.log(data);
+    this.setState({ retornoAPI: data });
+    // console.log(this.state.retornoAPI[0].artistName);
   }
 
   render() {
-    const { id } = this.props.match.params;
+    const { retornoAPI } = this.state;
+    // const { match } = this.props;
+    // const { id } = match.params;
     return (
       <div data-testid="page-album">
         <Header />
-        Album e
-        { id }
+        <p data-testid="artist-name">
+          {retornoAPI[0]?.artistName}
+        </p>
+        <p data-testid="album-name">{retornoAPI[0]?.collectionName}</p>
+        { retornoAPI.filter((el, _i, arr) => el !== arr[0])
+          .map((element, index) => (<MusicCard
+            trackName={ element.trackName }
+            previewUrl={ element.previewUrl }
+            key={ index }
+          />))}
       </div>
     );
   }
 }
-// validation
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
